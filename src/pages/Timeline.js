72737a4fc7f1b2f45ecea2ab33cd293867c673be
SemihDurmus <semihdurmus@yahoo.com>
2020-12-14 09:59:46 +0100

    Connecting realtime DB

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import auth from '@react-native-firebase/auth';
+import moment from 'moment';
 import database from '@react-native-firebase/database';
 import {
   SafeAreaView,
@@ -13,9 +14,33 @@ import {timelinePage} from './styles';
 //import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
 import {Header, PostInput, TopicSelectModal} from '../components';
 
+const user = auth().currentUser ? auth().currentUser : 'Default';
+
 const Timeline = () => {
   const [topicModalFlag, setTopicModalFlag] = useState(true);
   const [selectedTopic, setSelectedTopic] = useState(null);
+
+  const selectingTopic = (val) => {
+    setSelectedTopic(val);
+    setTopicModalFlag(false);
+
+    database()
+      .ref()
+      .on('value', (snapshot) => {
+        console.log('Data: ', snapshot.val());
+      });
+  };
+
+  const sendingPost = (value) => {
+    const postObject = {
+      userMail: user.email,
+      postText: value,
+      time: moment().toISOString(),
+    };
+
+    database().ref(`${selectedTopic}/`).push(postObject);
+  };
+
   return (
     <SafeAreaView style={timelinePage.container}>
       <KeyboardAvoidingView
@@ -32,17 +57,14 @@ const Timeline = () => {
               null;
             }}
           />
-          <PostInput onSendPost={(val) => console.log(val)} />
+          <PostInput onSendPost={sendingPost} />
 
           <TopicSelectModal
             visibility={topicModalFlag}
             onClose={() =>
               selectedTopic !== null ? setTopicModalFlag(false) : null
             }
-            onTopicSelect={(val) => {
-              setSelectedTopic(val);
-              setTopicModalFlag(false);
-            }}
+            onTopicSelect={selectingTopic}
           />
         </View>
       </KeyboardAvoidingView>
